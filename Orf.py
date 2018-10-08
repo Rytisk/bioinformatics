650a29dcef934e6dee5a80cc6ad112f31e581e5f
rytisk <rytis.kap@gmail.com>
2018-10-08 22:09:03 +0300

    Fixed ORF search

--- Orf.py
+++ Orf.py
@@ -1,8 +1,10 @@
 class Orf:    
-    def __init__(self, start, stop, value):
+    def __init__(self, start, stop, frame, value, length):
         self.start = start
         self.stop = stop
         self.value = value
+        self.frame = frame
+        self.length = length
     
     def __str__(self):
-        return 'ORF <Start: {}, Stop: {}>'.format(self.start, self.stop)
+        return 'ORF <Start: {}, Stop: {}, Length: {}, Frame: {}>'.format(self.start, self.stop, self.length, self.frame)
