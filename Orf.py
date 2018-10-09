64f43c9aab04a0d4f5c0627f44d6cdb9434efdea
rytisk <rytis.kap@gmail.com>
2018-10-10 00:32:25 +0300

    calculating distances

--- Orf.py
+++ Orf.py
@@ -1,10 +1,11 @@
 class Orf:    
-    def __init__(self, start, stop, frame, value, length):
+    def __init__(self, start, stop, frame, value, length, name):
         self.start = start
         self.stop = stop
         self.value = value
         self.frame = frame
         self.length = length
+        self.name = name
     
     def __str__(self):
-        return 'ORF <Start: {}, Stop: {}, Length: {}, Frame: {}>'.format(self.start, self.stop, self.length, self.frame)
+        return '{} <Start: {}, Stop: {}, Length: {}, Frame: {}>'.format(self.name, self.start, self.stop, self.length, self.frame)
